e2a2e37bb7531a06b61a3b287c4c41781297eb66
Viacheslav Verkhogliad <verkhogliad.v@gmail.com>
2016-08-19 04:26:19 +0300

    activate mail.php

--- mail.php
+++ mail.php
@@ -1,12 +1,15 @@
 <?php
 
 $frm_name  = "Youname"
-$recepient = "youmail@mail.ru";
-$sitename  = "Портфолио фотографий";
-$subject   = "Новая заявка с сайта \"$sitename\"";
+$recepient = "vkgroupcompany@gmail.com";
+$sitename  = "Tesla";
+$subject   = "new question or comment \"$sitename\"";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
+$phone = trim($_POST["phone"]);
 $message = trim($_POST["message"]);
-$message = "E-mail: $email \nИмя: $name \nСообщение: $message";
-mail($recepient, $subject, $message, "From: $frm_name <$recepient>" . "\r\n" . "Reply-To: $recepient" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
+$message = "E-mail: $email \n Name: $name \n Phone: $phone \n Message: $message";
+mail($recepient, $subject, $message,
+    "From: $frm_name <$recepient>" . "\r\n" . "Reply-To: $recepient" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html;
+     charset=\"utf-8\"");
